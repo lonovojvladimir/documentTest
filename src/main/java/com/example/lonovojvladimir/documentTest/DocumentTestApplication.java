a1c308c5bf38e1feb5ebc62d94e3d98e4227b7f5
lonovojvladimir <lonovojvladimir@icloud.com>
2026-02-02 00:45:02 +0300

    first commit

---
+++ src/main/java/com/example/lonovojvladimir/documentTest/DocumentTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.lonovojvladimir.documentTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lonovojvladimir/documentTest/DocumentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lonovojvladimir.documentTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
